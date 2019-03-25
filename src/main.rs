67b2ba8de0c7a61121f7c2c5e907dcdbf5d894af
ZapAnton <wc3ft2@gmail.com>
2019-03-25 13:05:56 +0300

    Added basic clap setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::{App, Arg, ArgMatches};
+
+fn init_app<'a>() -> ArgMatches<'a> {
+    App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .arg(
+            Arg::with_name("slug")
+                .short("s")
+                .long("slug")
+                .help("The slug of the exercise to be analyzed (e.g. 'reverse-string').")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("path")
+                .short("p")
+                .long("path")
+                .help("A path to a directory containing the submitted file(s).")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _matches = init_app();
 }
