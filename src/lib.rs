f7d00018a36fa184c7afbcd6faf6380bfb3925c6
ZapAnton <wc3ft2@gmail.com>
2019-03-25 15:45:42 +0300

    Added the 'analysis.json' file creation

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,17 @@
-use std::path::Path;
+use std::{fs, path::Path};
+use AnalysisStatus::*;
 use ExerciseType::*;
 
 enum ExerciseType {
     ReverseString,
     Unknown,
+}
+
+enum AnalysisStatus {
+    ApproveAsOptimal,
+    ApproveWithComment,
+    DisapproveWithComment,
+    ReferToMentor,
 }
 
 impl From<&str> for ExerciseType {
@@ -13,6 +21,32 @@ impl From<&str> for ExerciseType {
             _ => Unknown,
         }
     }
+}
+
+impl From<AnalysisStatus> for &str {
+    fn from(status: AnalysisStatus) -> Self {
+        match status {
+            ApproveAsOptimal => "approve_as_optimal",
+            ApproveWithComment => "approve_with_comment",
+            DisapproveWithComment => "disapprove_with_comment",
+            ReferToMentor => "refer_to_mentor",
+        }
+    }
+}
+
+fn write_analysis_result(
+    exercise_dir_path: &Path,
+    status: AnalysisStatus,
+    _comments: &[&str],
+) -> Result<(), std::io::Error> {
+    let analysis_file_path = exercise_dir_path.join("analysis.json");
+
+    let analysis_content = format!(
+        "{{\n    \"status\": \"{}\",\n    \"comments\": []\n}}",
+        Into::<&str>::into(status)
+    );
+
+    fs::write(analysis_file_path, analysis_content)
 }
 
 pub fn analyze_exercise(slug: &str, path: &str) -> Result<(), std::io::Error> {
@@ -35,5 +69,5 @@ pub fn analyze_exercise(slug: &str, path: &str) -> Result<(), std::io::Error> {
         return Ok(());
     }
 
-    Ok(())
+    write_analysis_result(&exercise_dir_path, ReferToMentor, &[""])
 }
