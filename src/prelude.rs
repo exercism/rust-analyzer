6db1c251ddb11237307561f9f8af22cda846121b
Giles Cope <gilescope@gmail.com>
2021-01-12 12:55:51 +0000

    Tidied imports

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,12 +1,6 @@
-pub use crate::analyzers::{output::AnalysisOutput, Analyze};
-pub use crate::Result;
-
-pub use crate::bad;
-pub use crate::good;
-pub use crate::note;
-
-pub use crate::bad_if_missing;
-pub use crate::good_if_missing;
-pub use crate::note_if_missing;
+pub use crate::{
+    analyzers::{output::AnalysisOutput, Analyze},
+    bad, bad_if_missing, good, good_if_missing, note, note_if_missing, Result,
+};
 
 pub use syn::File;
