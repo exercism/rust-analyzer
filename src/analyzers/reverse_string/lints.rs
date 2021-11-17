2e1e84ced208eccdb13cb06558a59464aeacaf95
Peter Tseng <petertseng@users.noreply.github.com>
2021-11-17 07:26:17 +0000

    rust-analyzer 0.4.2: use syn 1.0 (#39)
    It was suggested in https://github.com/exercism/rust-analyzer/issues/38
    that perhaps syn 1.0 would allow the analyzer to parse student
    submissions again.
    
    **WARNINGS!!!**
    
    * The author of this commit has zero knowledge of the `syn` crate or of
      the `rust-analyzer`.
    * The author of this commit has no way to verify that these changes
      allow the `rust-analyzer` to successfully analyse a submitted student
      solution; if they don't, these changes failed to achieve their
      intended objective.
    
    The only indication that these changes are even plausible is that the
    tests pass, but the author also does not know how comprehensive the
    tests are.
    
    Because the author has no way of verifying it, the author cannot in good
    conscience say that this commit closes the aforementioned issue; it will
    have to be manually checked and closed by a knowledgeable individual.
    
    These are simply the changes blindly applied from syn 1.0, as follows.
    
    All top-level bullet points are direct quotes from
    https://github.com/dtolnay/syn/releases/tag/1.0.0.
    All second-level bullets are not direct quotes but instead commentary
    from the author of this commit.
    
    * The `first`, `last`, and `last_mut` accessors of `Punctuated` now
      return the element `T` only, no longer a punctuated pair.
      https://github.com/dtolnay/syn/commit/d919fb64d6c23a057976bc22b80b6fddb29862a9
      * It is the judgment of the author of this commit that the analyzer is
        not interested in checking the punctuation and only needs to check
        the path element, so `first` was used instead of `pairs`.
    * The syntax tree for function arguments has been redesigned. The new
      type is `FnArg`.
      https://github.com/dtolnay/syn/commit/ae32af4b4d8fe9083c3aa4d0da6302390b8737b7
      * As a direct consequence, match of `FnArg::Captured(ArgCaptured(..))`
        instead must become `FnArg::Typed(PatType)`
      * Unknown whether direct consequence: PatType's `ty` is `Box<Type>`
        instead of `Type`, and `pat` is `Box<Pat>` instead of `Pat`,
        therefore all pattern matches against `ty` and `pat` require an
        extra level of destructuring.
    * The syntax tree for function signatures has been redesigned. The types
      `MethodSig` and `FnDecl` have been unified into one type `Signature`
      that is common across all varieties of function signatures.
      * https://github.com/dtolnay/syn/commit/c7369974dfaf3e79a7c809e35127276ff6326049
      * As a direct consequence, field `decl` is now field `sig`.
      * As a direct consequence, `ident` moves into `sig`.

--- src/analyzers/reverse_string/lints.rs
+++ src/analyzers/reverse_string/lints.rs
@@ -36,7 +36,7 @@ fn check_for_extern_crate(ast: &File, output: &mut AnalysisOutput) -> Result<()>
 fn check_for_standard_solution(ast: &File, output: &mut AnalysisOutput) -> Result<()> {
     let reverse_fn = if let Some(syn::Item::Fn(func)) = ast.items.iter().find(|item| {
         if let syn::Item::Fn(item_fn) = item {
-            item_fn.ident == "reverse"
+            item_fn.sig.ident == "reverse"
         } else {
             false
         }
@@ -71,17 +71,15 @@ fn check_for_standard_solution(ast: &File, output: &mut AnalysisOutput) -> Resul
                     if method != "chars" {
                         false
                     } else if let syn::Expr::Path(syn::ExprPath { path, .. }) = receiver.as_ref() {
-                        if let Some(syn::punctuated::Pair::End(path_segment)) =
-                            path.segments.first()
-                        {
-                            if let Some(syn::punctuated::Pair::End(syn::FnArg::Captured(
-                                syn::ArgCaptured {
-                                    pat: syn::Pat::Ident(syn::PatIdent { ident, .. }),
-                                    ..
-                                },
-                            ))) = reverse_fn.decl.inputs.first()
+                        if let Some(path_segment) = path.segments.first() {
+                            if let Some(syn::FnArg::Typed(syn::PatType { pat, .. })) =
+                                reverse_fn.sig.inputs.first()
                             {
-                                *ident == path_segment.ident
+                                if let syn::Pat::Ident(syn::PatIdent { ref ident, .. }) = **pat {
+                                    *ident == path_segment.ident
+                                } else {
+                                    false
+                                }
                             } else {
                                 false
                             }
@@ -113,7 +111,7 @@ fn check_for_standard_solution(ast: &File, output: &mut AnalysisOutput) -> Resul
 fn check_for_optimal_solution(ast: &File, output: &mut AnalysisOutput) -> Result<()> {
     let reverse_fn = if let Some(syn::Item::Fn(func)) = ast.items.iter().find(|item| {
         if let syn::Item::Fn(item_fn) = item {
-            item_fn.ident == "reverse"
+            item_fn.sig.ident == "reverse"
         } else {
             false
         }
@@ -150,24 +148,22 @@ fn check_for_optimal_solution(ast: &File, output: &mut AnalysisOutput) -> Result
                 {
                     if method != "graphemes" {
                         false
-                    } else if let Some(syn::punctuated::Pair::End(syn::Expr::Lit(syn::ExprLit {
+                    } else if let Some(syn::Expr::Lit(syn::ExprLit {
                         lit: syn::Lit::Bool(syn::LitBool { value, .. }),
                         ..
-                    }))) = args.first()
+                    })) = args.first()
                     {
                         *value
                     } else if let syn::Expr::Path(syn::ExprPath { path, .. }) = receiver.as_ref() {
-                        if let Some(syn::punctuated::Pair::End(path_segment)) =
-                            path.segments.first()
-                        {
-                            if let Some(syn::punctuated::Pair::End(syn::FnArg::Captured(
-                                syn::ArgCaptured {
-                                    pat: syn::Pat::Ident(syn::PatIdent { ident, .. }),
-                                    ..
-                                },
-                            ))) = reverse_fn.decl.inputs.first()
+                        if let Some(path_segment) = path.segments.first() {
+                            if let Some(syn::FnArg::Typed(syn::PatType { pat, .. })) =
+                                reverse_fn.sig.inputs.first()
                             {
-                                *ident == path_segment.ident
+                                if let syn::Pat::Ident(syn::PatIdent { ref ident, .. }) = **pat {
+                                    *ident == path_segment.ident
+                                } else {
+                                    false
+                                }
                             } else {
                                 false
                             }
